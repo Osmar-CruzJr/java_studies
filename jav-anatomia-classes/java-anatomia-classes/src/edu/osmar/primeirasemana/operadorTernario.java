5975ae8ec3cb2c025d9545e3fa8ac4c003a73405
osmarjunior <osmarnunescruzjunior@outlook.com>
2025-03-08 11:10:04 -0300

    Operador ternário

---
+++ jav-anatomia-classes/java-anatomia-classes/src/edu/osmar/primeirasemana/operadorTernario.java
@@ -0,0 +1,13 @@
+package edu.osmar.primeirasemana;
+
+public class operadorTernario {
+    public static void main(String[] args) {
+        int a, b;
+        a = 6;
+        b = 7;
+
+        int resultado = a == b ? 1 : 0;
+
+        System.out.println(resultado);
+    }
+}
